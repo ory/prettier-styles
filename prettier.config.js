5453e96ee464f6cbd2e69466d5df9e5a4ccd3783
Kevin Goslar <kevin.goslar@gmail.com>
2022-06-10 02:21:47 -0500

    ci: format all files known to Prettier and keep them formatted (#40)

--- prettier.config.js
+++ prettier.config.js
@@ -1,17 +1,17 @@
-"use strict";
-exports.__esModule = true;
+'use strict'
+exports.__esModule = true
 var options = {
-    trailingComma: 'none',
-    semi: false,
-    singleQuote: true,
-    proseWrap: 'always',
-    overrides: [
-        {
-            files: ['*.md', '*.mdx'],
-            options: {
-                printWidth: 130
-            }
-        }
-    ]
-};
-module.exports = options;
+  trailingComma: 'none',
+  semi: false,
+  singleQuote: true,
+  proseWrap: 'always',
+  overrides: [
+    {
+      files: ['*.md', '*.mdx'],
+      options: {
+        printWidth: 130
+      }
+    }
+  ]
+}
+module.exports = options
