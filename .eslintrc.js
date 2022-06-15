dfce99516a124b2b32c37f53abaf007479c47cb8
Kevin Goslar <kevin.goslar@gmail.com>
2022-06-15 18:12:43 -0500

    Simplify

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,6 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   root: true,
   rules: {
-    'sort-keys': ['error', 'asc', { caseSensitive: true, natural: false }]
+    'sort-keys': ['error']
   }
 }
