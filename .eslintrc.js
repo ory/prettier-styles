52498d301f14d6c57141040e97ee08a88c2f5ffe
Kevin Goslar <kevin.goslar@gmail.com>
2022-06-18 10:41:53 -0500

    feat: use default value for quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,14 @@ module.exports = {
     node: true
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier' // this must come last
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier" // this must come last
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   root: true,
   rules: {
-    'sort-keys': ['error']
+    "sort-keys": ["error"]
   }
 }
